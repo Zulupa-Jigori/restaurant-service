c9cf4415831cfe449f43420d27c272d21ce116ea
maca-js <71559860+maca-js@users.noreply.github.com>
2023-07-19 10:48:18 +0300

    connect swagger (#4)
    Co-authored-by: Maksym Tkachuk <maksym.tkachuk@infopulse.com>

--- src/dishes/dishes.controller.ts
+++ src/dishes/dishes.controller.ts
@@ -7,35 +7,42 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { DishesService } from './dishes.service';
 import { CreateDishDto } from './dto/create-dish.dto';
 import { UpdateDishDto } from './dto/update-dish.dto';
 
+@ApiTags('dishes')
 @Controller('dishes')
 export class DishesController {
   constructor(private readonly dishesService: DishesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create dish' })
   create(@Body() createDishDto: CreateDishDto) {
     return this.dishesService.create(createDishDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all dishes' })
   findAll() {
     return this.dishesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get specific dish' })
   findOne(@Param('id') id: string) {
     return this.dishesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update specific dish' })
   update(@Param('id') id: string, @Body() updateDishDto: UpdateDishDto) {
     return this.dishesService.update(+id, updateDishDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete specific dish' })
   remove(@Param('id') id: string) {
     return this.dishesService.remove(+id);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Restaurant API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   app.enableCors();
   await app.listen(3000);
 }
