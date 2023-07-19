c40ba56177eba56372a1347889b7f1ff5b7e128b
maca-js <71559860+maca-js@users.noreply.github.com>
2023-07-19 10:27:17 +0300

    Feature/db connection (#2)
    * added connection to db
    
    * convert CRLF to LF
    
    * rename dish into dishes
    
    ---------
    
    Co-authored-by: Maksym Tkachuk <maksym.tkachuk@infopulse.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DishesModule } from './dishes/dishes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [`${__dirname}/**/*.entity{.js, .ts}`],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    DishesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
